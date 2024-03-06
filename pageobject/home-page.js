1d60920123a7b85bc84709f3b45b3441fe3c9416
Carlos Aquije Melgar <enrique.a.melgar@gmail.com>
2024-03-05 23:19:50 -0500

    Created homepage and starting test for homepage

---
+++ pageobject/home-page.js
@@ -0,0 +1,15 @@
+const { expect } = require('@playwright/test');
+
+exports.HomePage = class HomePage {
+    /**
+     * @param {import('@playwright/test').Page} page
+     */
+    constructor(page) {
+        this.page = page;
+        this.logo = page.locator('.Logo-module_logo__iW4zn');
+    }
+
+    async goto() {
+        await this.page.goto('https://linio.falabella.com.pe/linio-pe');
+    }
+}
