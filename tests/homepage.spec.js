37bf06590f0c412ed4d6c6063733b3ba7dcb4fd4
Carlos Aquije Melgar <enrique.a.melgar@gmail.com>
2024-03-05 23:45:27 -0500

    homepage extended with searchBar and search function

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -1,9 +1,17 @@
 const { test, expect } = require('@playwright/test');
 const { HomePage } = require('../pageobject/home-page');
 
-test('should show Linio Icon', async ({ page }) => {
+test('should show Linio logo', async ({ page }) => {
     const homepage = new HomePage(page);
     await homepage.goto();
 
     await expect(homepage.logo).toBeVisible();
-})
+});
+
+test('should searchBar contains text', async ({ page }) => {
+   const homePage = new HomePage(page);
+   await homePage.goto();
+   await homePage.searchProduct('macbook');
+
+   await expect(homePage.searchBar).toHaveValue('macbook');
+});
